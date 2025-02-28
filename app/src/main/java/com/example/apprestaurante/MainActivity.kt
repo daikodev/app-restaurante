2cbb5a5899df2082fc4fd8cf69629e18c57c02bf
DaikoDev <cdanieleon28@gmail.com>
2025-02-27 20:22:31 -0500

    Cambiar nombres de botones de cerrar la aplicacion

--- app/src/main/java/com/example/apprestaurante/MainActivity.kt
+++ app/src/main/java/com/example/apprestaurante/MainActivity.kt
@@ -127,7 +127,7 @@ class MainActivity : AppCompatActivity() {
         val builder: AlertDialog.Builder = AlertDialog.Builder(this)
         builder
             .setTitle("¿Desea salir de la aplicación?")
-            .setPositiveButton(android.R.string.yes) { dialog, which ->
+            .setPositiveButton("Sí") { dialog, which ->
                 Toast.makeText(
                     applicationContext, android.R.string.yes,
                     Toast.LENGTH_SHORT
@@ -135,7 +135,7 @@ class MainActivity : AppCompatActivity() {
 
                 finishAffinity()
             }
-            .setNegativeButton(android.R.string.no) { dialog, which ->
+            .setNegativeButton("No") { dialog, which ->
                 Toast.makeText(
                     applicationContext, android.R.string.no,
                     Toast.LENGTH_SHORT
